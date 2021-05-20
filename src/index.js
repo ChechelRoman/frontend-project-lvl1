045ab02c546d13cdd2323976b2bd1c274defc943
Chechel Roman <chechel-roman@lenta.ru>
2021-05-20 17:51:44 +0700

    moved game engine to its own module

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import readlineSync from 'readline-sync';
+import gameEngine from './game-engine.js';
 
-export default (gameData, description) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(description);
-  const maxRounds = 3;
-
-  for (let i = 0; i < maxRounds; i += 1) {
-    const [question, correctAnswer] = gameData();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-
-    console.log('Correct!');
-  }
-
-  console.log(`Congratulations, ${userName}!`);
-};
+export default gameEngine;
